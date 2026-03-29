c3339f9018807a4d7869ae74fd0b6087ffc775a8
edwinhuguito-dev <edwinhuguito@gmail.com>
2026-03-28 20:58:29 -0500

    mas actuliazacon con bcrypt

--- controllers/registerUsuario.controller.js
+++ controllers/registerUsuario.controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const registerUsuario = require("../models/registerUsuario");
 
 const getRegister = async (req, res) =>{
@@ -13,11 +14,21 @@ const getRegister = async (req, res) =>{
 
 const postRegister = async (req,res) =>{
     try{
-        const newRegister = await registerUsuario.create(req.body);
-        res.status(201).json(newRegister);
-    }catch (error){
-        console.log(error);
-        res.status(400).json({mensaje: "No se pudo crear usuario", detalle: error.message});
+        const {name, nickname, avatarUrl, phone, email, password} = req.body;
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const newRegister = await registerUsuario.create({
+            name, nickname, avatarUrl, phone, email, password: hashedPassword});
+        res.status(201).json({
+            mensaje: "Usuario registrado con éxito",
+            usuario: newRegister});
+        }catch (error){
+            console.log(error);
+            res.status(400).json({mensaje:"No se pudo crear usuario",
+                                    detalle: error.message});
+
     }
 };
 
